ae81f3f975d179a7aa99862931080a2aa63ec5ea
Matteo Prosperi <maprospe@microsoft.com>
2021-12-28 11:16:13 -0800

    Proof of concept of Visual Studio language server extension

---
+++ editors/vs/RustLspClient/RustContentTypeDefinition.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace MockLanguageExtension
+{
+    public class RustContentDefinition
+    {
+        [Export]
+        [Name("rs")]
+        [BaseDefinition(CodeRemoteContentDefinition.CodeRemoteContentTypeName)]
+        internal static ContentTypeDefinition RsContentTypeDefinition;
+
+
+        [Export]
+        [FileExtension(".rs")]
+        [ContentType("rs")]
+        internal static FileExtensionToContentTypeDefinition RsFileExtensionDefinition;
+    }
+}
---
+++ editors/vs/RustLspClient/RustLanguageClient.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using Microsoft.VisualStudio.Threading;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RustLspClient
+{
+    [ContentType("rs")]
+    [Export(typeof(ILanguageClient))]
+    public class RustLanguageClient : ILanguageClient
+    {
+        public string Name => "Rust Language Extension";
+
+        public IEnumerable<string> ConfigurationSections => null;
+
+        public object InitializationOptions => null;
+
+        public IEnumerable<string> FilesToWatch => null;
+
+        public bool ShowNotificationOnInitializeFailed => true;
+
+        public event AsyncEventHandler<EventArgs> StartAsync;
+        public event AsyncEventHandler<EventArgs> StopAsync;
+
+        public async Task<Connection> ActivateAsync(CancellationToken token)
+        {
+            await Task.Yield();
+
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"rust-analyzer\rust-analyzer.exe");
+            info.RedirectStandardInput = true;
+            info.RedirectStandardOutput = true;
+            info.UseShellExecute = false;
+            info.CreateNoWindow = true;
+
+            Process process = new Process();
+            process.StartInfo = info;
+
+            if (process.Start())
+            {
+                return new Connection(process.StandardOutput.BaseStream, process.StandardInput.BaseStream);
+            }
+
+            return null;
+        }
+
+        public async Task OnLoadedAsync()
+        {
+            await StartAsync?.InvokeAsync(this, EventArgs.Empty);
+        }
+
+        public Task OnServerInitializedAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task<InitializationFailureContext> OnServerInitializeFailedAsync(ILanguageClientInitializationInfo initializationState)
+        {
+            var result = initializationState.InitializationException?.ToString() ?? "null";
+            return new InitializationFailureContext() { FailureMessage = result };
+        }
+    }
+}
